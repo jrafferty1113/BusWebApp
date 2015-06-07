b87f9192ec3e083d7ae4b82afd1601cee263e249
Fiona Lu <glu@livongo.com>
2015-06-07 13:21:50 -0700

    Change RouteService

--- app/services/RouteService.java
+++ app/services/RouteService.java
@@ -5,10 +5,12 @@ import java.util.Set;
 import org.w3c.dom.Document;
 
 import models.bus.Direction;
+import models.bus.Route;
 
 public interface RouteService {
 
-	public void updateRoutes();
-	public Set<Direction> parseRouteXml(Document xml);
+	public void loadRoutes();
+	public Set<Route> getRoutes(Document xml);
+	public Set<Direction> getDirections(Document xml);
 	
 }
--- app/services/RouteServiceImpl.java
+++ app/services/RouteServiceImpl.java
@@ -7,20 +7,48 @@ import java.util.Set;
 
 import models.bus.Agency;
 import models.bus.Direction;
+import models.bus.Route;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.NodeList;
 
 public class RouteServiceImpl implements RouteService {
 
 	@Override
-	public void updateRoutes() {
+	public void loadRoutes() {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public Set<Direction> parseRouteXml(Document xml) {
+	public Set<Route> getRoutes(Document xml) {
+		NodeList nl = xml.getElementsByTagName("body").item(0).getChildNodes();
+		
+		Set<Route> routes = new HashSet<Route>();
+		
+		for (int i = 0; i < nl.getLength(); i++) {
+			if (nl.item(i).getNodeName().equals("route")) {
+				
+				NamedNodeMap attributes = nl.item(i).getAttributes();
+				Route r = new Route();
+				
+				r.setTag(attributes.getNamedItem("tag").getNodeValue());
+				r.setTitle(attributes.getNamedItem("title").getNodeValue());
+				r.setLatMax(Double.valueOf(attributes.getNamedItem("latMax").getNodeValue()));
+				r.setLatMin(Double.valueOf(attributes.getNamedItem("latMin").getNodeValue()));
+				r.setLonMax(Double.valueOf(attributes.getNamedItem("lonMax").getNodeValue()));
+				r.setLonMin(Double.valueOf(attributes.getNamedItem("lonMin").getNodeValue()));
+				
+				routes.add(r);
+			}
+		}
+		
+		return routes;
+	}
+	
+	@Override
+	public Set<Direction> getDirections(Document xml) {
 		NodeList nl = xml.getElementsByTagName("body").item(0).getChildNodes().item(0).getChildNodes();
 		play.Logger.error(nl.item(0).getNodeName());
 		
@@ -34,5 +62,4 @@ public class RouteServiceImpl implements RouteService {
 //		}
 		return list;
 	}
-
 }
